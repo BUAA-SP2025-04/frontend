7815ab970da79393cba8b1563d4eb63a5ce2ee31
cjDaBaoCai <2962499353@qq.com>
2025-07-01 13:54:53 +0800

    feat: 关注词条适配新格式; 添加发现的文献相关类型和接口; 完善词条管理界面 refactor: 发现文献相关类型和函数移至单独文件

---
+++ src/api/types/discoverLiterature.ts
@@ -0,0 +1,47 @@
+export interface DiscoverTerm {
+  id: string
+  value: string
+}
+
+export interface DiscoverTermGroup {
+  title: DiscoverTerm[]
+  author: DiscoverTerm[]
+  field: DiscoverTerm[]
+}
+
+export interface DiscoverLiterature {
+  id: string
+  sid: string
+  arxivId: string
+  title: string
+  summary: string
+  authors: string[]
+  link: string
+  pdf: string
+  primaryCategory: string
+  categories: string[]
+  published: string
+}
+
+export interface DiscoverLiteratureGroup {
+  title: DiscoverLiterature[]
+  author: DiscoverLiterature[]
+  field: DiscoverLiterature[]
+}
+
+export interface UpdateTermRequest {
+  addedTerm: DiscoverTermGroup
+  deletedTerm: DiscoverTermGroup
+}
+
+export interface DiscoverTermGroupResponse {
+  code: string
+  message: string
+  data: DiscoverTermGroup
+}
+
+export interface DiscoverLiteratureResponse {
+  code: string
+  message: string
+  data: DiscoverLiteratureGroup
+}
